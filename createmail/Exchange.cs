00dd07ec53572f474b4656354db025267416ba6c
Jason Fenech <jason.fenech@altaro.com>
2020-03-09 15:02:36 +0100

    Add project files.

---
+++ createmail/Exchange.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Exchange.WebServices.Data;
+using Task = System.Threading.Tasks.Task;
+using RandomDataGenerator.Randomizers;
+using RandomDataGenerator.FieldOptions;
+
+namespace createmail
+{
+    public class Exchange
+    {
+        private int maxThreads { get; set; }
+        private int maxItemsCreatedPerThread { get; set; }
+        private string domain { get; set; }
+        private string pwd { get; set; }
+        private string smtp { get; set; }
+        private string configUrl { get; set; }
+        public FolderId folderid { get; set; }
+        private ExchangeService service { get; set; }
+
+        public List<ServiceResponseCollection<ServiceResponse>> results = new List<ServiceResponseCollection<ServiceResponse>>();
+
+        public Exchange(int maxThreads, int maxItemsPerThread, string domain, string smtp, string pwd, string configUrl)
+        {
+            this.maxThreads = maxThreads;
+            this.maxItemsCreatedPerThread = maxItemsPerThread;
+            this.domain = domain;
+            this.smtp = smtp;
+            this.pwd = pwd;
+            this.configUrl = configUrl;
+        }
+
+        private bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public void Connect()
+        {
+            this.service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+            this.service.Credentials = new WebCredentials(smtp, pwd, domain);
+            this.service.AutodiscoverUrl(smtp, RedirectionUrlValidationCallback);
+        }
+
+        public void MultiThreadedCreateEmails()
+        {
+            var tasks = new Task[maxThreads];
+            for (int i = 0; i < maxThreads; i++)
+            {
+                tasks[i] = new Task((object param) =>
+                {
+                    var j = (int)param;
+                    Console.WriteLine($"Task {j} started ...");
+                    results.Add(this.service.CreateItems(GenerateListOfEmailItems(j), GetFolderId("inbox"), MessageDisposition.SaveOnly, null));
+                    Console.WriteLine($"Task {j} exiting ...");
+                }, i);
+            }
+
+            Parallel.ForEach<Task>(tasks, (t) => { t.Start(); });
+            Task.WaitAll(tasks);
+        }
+
+        private List<Item> GenerateListOfEmailItems(int seed)
+        {
+            List<Item> emails = new List<Item>();
+
+            string GenEmailBody()
+            {
+                return new RandomizerText(new FieldOptionsText
+                {
+                    Min = 100,
+                    Max = 1000,
+                    UseSpecial = true,
+                    UseLetter = true,
+                    UseNumber = true,
+                    UseLowercase = true,
+                    UseSpace = true
+                }).Generate();
+            }
+
+            string GenEmailAddress()
+            {
+                return new RandomizerEmailAddress(new FieldOptionsEmailAddress
+                {
+                    Male = true,
+                    Female = true
+
+                }).Generate();
+            }
+
+            for (int x = 0; x < this.maxItemsCreatedPerThread; x++)
+            {
+                EmailMessage emailMsg = new EmailMessage(this.service);
+                emailMsg.From = new EmailAddress(GenEmailAddress());
+                emailMsg.ToRecipients.Add(GenEmailAddress());
+                //emailMsg.Subject = $"Created-By-Thread{seed}-{new Random((x+1)*10).Next(11111111,99999999)}-{DateTime.Now.Ticks.ToString()+(x+1)*10}";
+                emailMsg.Subject = new RandomDataGenerator.Randomizers.RandomizerText(new FieldOptionsText { Min = 32, Max = 64, UseLetter = true, UseLowercase = true,
+                    UseNumber = true, UseUppercase = true, UseNullValues = false, UseSpace = false, UseSpecial = false }).Generate();
+                emailMsg.Body = GenEmailBody();
+                emailMsg.IsRead = false;
+                emails.Add(emailMsg);
+            }
+
+            return emails;
+        }
+
+        public FolderId GetFolderId(string folderName)
+        {
+            FolderId id = null;
+
+            this.service.Url = new Uri(configUrl);
+            FolderView view = new FolderView(1000);
+            view.PropertySet = new PropertySet(BasePropertySet.IdOnly);
+            view.PropertySet.Add(FolderSchema.DisplayName);
+            view.Traversal = FolderTraversal.Deep;
+            FindFoldersResults findFolderResults = this.service.FindFolders(WellKnownFolderName.Root, view);
+
+            foreach (Folder f in findFolderResults)
+            {
+                if (f.DisplayName.ToLower().Equals(folderName.ToLower()))
+                {
+                    id = f.Id;
+                    break;
+                }
+            }
+            return id;
+        }
+    }
+}
---
+++ createmail/Program.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace createmail
+{
+    class Program
+    {
+        private static string domain;
+        private static string pwd;
+        private static string smtp;
+        private static string configUrl;
+        private static int iterations;
+        private static int maxItemsPerThread;
+        private static int maxThreads;
+
+        static int items = 0;
+        static int successful = 0;
+        static int warning = 0;
+        static int error = 0;
+        static int start = 0;
+        static int resume = 0;
+
+        static Stopwatch m = new Stopwatch();
+        static Exchange exchange;
+
+        static void ParseResults(List<ServiceResponseCollection<ServiceResponse>> results)
+        {
+            foreach (var coll in results)
+            {
+                foreach (var res in coll)
+                {
+                    items++;
+                    if (res.Result.ToString().ToLower().Contains("success")) successful++;
+                    else
+                    if (res.Result.ToString().Contains("warning")) warning++;
+                    else
+                    if (res.Result.ToString().Contains("error")) error++;
+                }
+            }
+        }
+
+        static void DoWork()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine();
+
+                ReadAppSettings();
+
+                Console.WriteLine($"Establishing connection to Exchange server");
+                exchange.Connect();
+                exchange.folderid = exchange.GetFolderId("INBOX");
+
+                m.Start();
+
+                Console.WriteLine($"Generating {iterations * maxItemsPerThread * maxThreads} emails for user {smtp}");
+                Console.WriteLine();
+
+                for (int i = start; i < iterations; i++)
+                {
+                    Console.WriteLine($"Pass {i + 1} : Saving {maxThreads * maxItemsPerThread} emails under folder Inbox");
+                    exchange.MultiThreadedCreateEmails();
+
+                    if (i % 20 == 0 && i != 0)
+                    {
+                        Console.WriteLine("Sleeping for 60 seconds");
+                        Thread.Sleep(60000);
+                    }
+
+                    if (i != iterations - 1 && i % 20 != 0)
+                    {
+                        Console.WriteLine("Sleeping for 10 seconds");
+                        Thread.Sleep(10000);
+                    }
+
+                    Console.WriteLine();
+                    resume = i;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exiting due to {ex.Message}");
+                Console.WriteLine($"Sleeping for 5 minutes before restarting ...");
+                Thread.Sleep(TimeSpan.FromMinutes(5));
+                start = resume;
+                DoWork();
+            }
+        }
+
+        public static void ReadAppSettings()
+        {
+            iterations = Int32.Parse(ConfigurationManager.AppSettings["Iterations"]);
+            maxItemsPerThread = Int32.Parse(ConfigurationManager.AppSettings["MaxItemsPerThread"]);
+            maxThreads = Int32.Parse(ConfigurationManager.AppSettings["MaxThreads"]);
+            smtp = ConfigurationManager.AppSettings["UserSMTP"];
+            pwd = ConfigurationManager.AppSettings["UserPassword"];
+            domain = ConfigurationManager.AppSettings["Domain"];
+            configUrl = @ConfigurationManager.AppSettings["ConfigUrl"];
+
+            exchange = new Exchange(maxThreads, maxItemsPerThread, domain, smtp, pwd, configUrl);
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                DoWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exiting due to {ex.Message}");
+            }
+            finally
+            {
+                m.Stop();
+                Console.WriteLine();
+
+                //Parse Results
+                ParseResults(exchange.results);
+
+                Console.WriteLine($"No. of items written : {items}");
+                Console.WriteLine($"No. of 'Success' ops : {successful}");
+                Console.WriteLine($"No. of 'Warning' ops : {warning}");
+                Console.WriteLine($"No. of 'Error' ops   : {error}");
+                Console.WriteLine($"Time taken           : {m.Elapsed.TotalSeconds} seconds");
+            }
+        }
+    }
+}
